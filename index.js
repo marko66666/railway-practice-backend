d533583022dbcbdb0573c4d90c7ce7ceaa0410cd
marko6666601~ <marko_susa@outlook.com>
2023-05-02 22:42:09 +0200

    add cors middleware

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const pool = require("./db");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/users", async (req, res) => {
   const users = await pool.query("SELECT * FROM users");
-  res.json(users.rows);
+  res.json(users);
 });
 
 const PORT = process.env.PORT || 5000;
